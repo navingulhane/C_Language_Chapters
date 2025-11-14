a6e574118dcf37669c337202d7fc91e87e3f76e5
Dogesh_badmosh ;) <navingulhane007@gmail.com>
2025-11-15 00:02:25 +0530

    Update and rename 01_Chapter/Control_Statements/Selection_Statement(Condition-Statements) to 01_Chapter/Control_Statements.c

---
+++ 01_Chapter/Control_Statements.c
@@ -0,0 +1,28 @@
+Control Statements
+        |
+        |-------------------------------|
+        |                               |
+        |                               |
+ Selection Statements           Iterative Statements
+ (Condition Control)                   (Loops)
+        |                               |
+        |                               |
+   ---------------------          --------------------
+   |        |          |          |        |         |S
+   |        |          |          |        |         |
+  if    if–else   else-if      for     while    do–while
+   |
+   |
+  switch
+
+
+Control Statements
+        |
+        |
+   Jump Statements
+        |
+        |-----------------------------|
+        |             |               |
+      break        continue         goto
+        |
+      return
